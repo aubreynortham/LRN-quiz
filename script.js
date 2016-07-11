7fb1e50d7d699c4a3fec24a21efa44544c2fd474
AUBREY NORTHAM <AENORTHAM@GMAIL.COM>
2016-07-10 23:31:59 -0400

    added master plan document to keep track of user stories, task tracking, and content questions, and made basic formatting updates in the code

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
 console.log("looking good");
 
-})
+
+});
+
+/*pseudocode overview:
+company name links to LRN homepage
+button to apply for Pearl Home certification links to LRN Pearl Homes page
+button to begin quiz loads new page layout with first question displayed
+if incorrect answer is clicked, shake the box, then load answer page
+if incorrect answer is clicked, add nothing to the corner total
+if correct answer is clicked, make button glow, then load answer page
+if correct answer is clicked, add +1 to the corner total
+when user clicks next page button, next question is displayed
+on the last answer page, next page button is replaced with see results button
+when user clicks see results button, load results page featuring final corner total
+when user clicks share button, display link (of landing page) to copy
+*/
