76b9024df70ada32827fb4e7183247e738b10377
AUBREY NORTHAM <AENORTHAM@GMAIL.COM>
2016-07-15 14:29:47 -0400

    essentially all the pieces are there, just need to get them all to work together correctly

--- newScript.js
+++ newScript.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
+  var quizScore = 0;
 
+  for (var i = 0; i < questionArray.length; i++) {
+    var selection = new QuizQuestion (questionArray[i].question, questionArray[i].choices, questionArray[i].answer);
+    var selectionView = new QuestionView(selection);
+    selectionView.render();
+    //how do I tell it to recognize quiz view versus answer view?
+    if (we are on question/choice view...) {
+      $(".empty").append(selectionView.el);
+    } else { //if we are on answer view...
+      $(".empty").append(selectionView.el);
+    }
+  }
 });
+
+//where do I factor in the victory condition..
